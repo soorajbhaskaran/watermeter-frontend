f730606ec0eaf391866f993ce179af9be3dc253b
Sooraj Bhaskaran <sbkaiprath99@gmail.com>
2022-06-02 19:39:54 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-//import Login from './components/Login';
-// import { Routes , Route } from 'react-router';
 import {
   BrowserRouter as Router,
   Routes,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
-import Home from './components/Home';
 import Login from './components/Login';
 
 function App() {
@@ -16,9 +12,7 @@ function App() {
       <div>
         <Routes>
         <Route path='/' element={<Login/>} />
-
         </Routes>
-        
       </div>
     </Router>
   );
